6be4c51137b8cc13762954fd2ff5881e6ebd3f67
Prince Kumar Rastogi <33233160+princerastogi@users.noreply.github.com>
2023-01-09 21:25:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.subtitle('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.title('Kale, Spinach & Rocket Smoothie')
 streamlit.title('Hard-Boiled Free-Range Egg')
